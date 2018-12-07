77383963d3509628cf93397a903bba9417701a64
Jessy Hu <jesshuy12@gmail.com>
2018-12-07 20:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var Variable = []; 
+  for (i = 0; i < musicians.length; i++ ) {
+    Variable.push(musicians[i] + "plays" + instruments[i]);
+    }
+  return Variable
+}
